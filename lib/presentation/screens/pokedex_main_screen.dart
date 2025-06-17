d3126e74239c570508312fd883530b3c4a126d3f
Mario Valdivieso <g10vann1vch@gmail.com>
2025-06-17 18:47:24 -0500

    pokemon card extracted as widget, pokemon image implemented, missing paginator

--- lib/presentation/screens/pokedex_main_screen.dart
+++ lib/presentation/screens/pokedex_main_screen.dart
@@ -13,23 +13,30 @@ class _PokedexMainScreenState extends State<PokedexMainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 202, 239, 245),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        toolbarHeight: 40,
-      ),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 25,),
             Text('Pokedex',
               style: TextStyle(
                 fontSize: 34,
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
-              ),),
-              SizedBox(height: 20,),
-              PokemonCard(),
+              ),
+            ),
+            SizedBox(height: 10,),
+            Expanded(
+              child:  ListView.builder(
+                itemCount: 5,
+                
+                itemBuilder: (context, index) {
+                  int id = (index + 1);
+                  return PokemonCard(pokeId: id);
+                  }
+                ),
+            ),
           ],
         ),
       ),
--- lib/presentation/widgets/pokemon_card_widget.dart
+++ lib/presentation/widgets/pokemon_card_widget.dart
@@ -4,46 +4,64 @@ import 'package:pokedex_app/presentation/widgets/pokemon_type_widget.dart';
 class PokemonCard extends StatelessWidget {
   const PokemonCard({
     super.key,
+    required this.pokeId
   });
+
+  final int pokeId;
+
+  String getPokeImgUrl(int id){
+    String pokemonid = id.toString().padLeft(3, '0');
+
+    return 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/$pokemonid.png';
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(25),
-      height: 130,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 73, 208, 176),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Bulbasaur', style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,),
-              ),
-              SizedBox(height: 10,),
-              Row(
-                children: [
-                  PokemonTypeWidget(type: 'Grass', typeColor: Color.fromARGB(255, 85, 190, 76),),
-                  SizedBox(width: 8,),
-                  PokemonTypeWidget(type: 'Poison', typeColor: Color.fromARGB(255, 102, 81, 204)),
-                ],
-              )
-            ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        height: 130,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 117, 227, 240),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: List<BoxShadow>.generate(
+            3,
+            (index) => BoxShadow(
+            color: const Color.fromARGB(66, 0, 0, 0),
+            blurRadius: 2 * (index + 1),
+            offset: Offset(0,2 * (index + 1))
+            ),
           ),
-          Image.network('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
-            width: 90,
-            height: 90,
-            fit: BoxFit.cover,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Bulbasaur', style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,),
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    PokemonTypeWidget(type: 'Grass', typeColor: Color.fromARGB(255, 85, 190, 76),),
+                    SizedBox(width: 8,),
+                    PokemonTypeWidget(type: 'Poison', typeColor: Color.fromARGB(255, 102, 81, 204)),
+                  ],
+                )
+              ],
+            ),
+            Image.network(getPokeImgUrl(pokeId),
+              height: 90,
+              fit: BoxFit.cover,
             )
-        ],
-    
+          ],
+        ),
       ),
     );
   }
